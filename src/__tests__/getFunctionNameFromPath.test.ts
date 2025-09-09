1e6b440ab153a78847d8244eff3d2faf6af2b660
Md Rakibul Islam Rocky <mdrirocky08@outlook.com>
2025-09-09 18:21:24 +0300

    Update getFunctionNameFromPath.test.ts

--- src/__tests__/getFunctionNameFromPath.test.ts
+++ src/__tests__/getFunctionNameFromPath.test.ts
@@ -6,10 +6,6 @@ describe("getFunctionNameFromPath", () => {
     expect(getFunctionNameFromPath("/foo/bar/index.ts")).toBe("bar");
   });
 
-  it("returns file name when path points directly to a file", () => {
-    expect(getFunctionNameFromPath("foo.ts")).toBe("foo");
-  });
-
   it("returns directory name when given a directory path", () => {
     expect(getFunctionNameFromPath("/foo/bar")).toBe("bar");
   });
